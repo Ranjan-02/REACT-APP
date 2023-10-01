fb83cc9017a9ddf7a73424e919db15e1a8411b6d
Ranjan02/ <ranjanmaji098@gmail.com>
2023-10-01 23:38:24 +0530

    add react practics and delete file

--- calculator/src/App.jsx
+++ calculator/src/App.jsx
@@ -1,12 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ContuctPage from "./ContuctPege";
+import HomePage from "./HomePage";
 
-import './App.css'
 
 function App() {
 
 
   return (
     <>
-      <h1>hellow word</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<h1>About Component</h1>} />
+          <Route path="/contuct" element={<ContuctPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ calculator/src/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HomePage() {
+    return (
+        <div className="bg-gradient-to-r from-blue-800 to-indigo-900  min-h-screen">
+            <h1>this is home page</h1>
+        </div>
+    )
+}
+
+export default HomePage
