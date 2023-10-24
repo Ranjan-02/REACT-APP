87d5ca996f992dac0ec3ace8e1b238679c00dd55
Ranjan02/ <ranjanmaji098@gmail.com>
2023-10-25 02:23:20 +0530

    concrpt react router dom

--- context-api/src/App.jsx
+++ context-api/src/App.jsx
@@ -1,46 +1,12 @@
-import React, { createContext } from "react"
-import ChildC from "./components/ChildC";
-import Forms from "./components/Forms";
-import UseState from "./components/UseState";
-import UseCallback from "./components/UseCallback";
-import ChildA from "./components/ChildA";
 
 
-const data1 = createContext()
-const data2 = createContext()
-const data3 = createContext()
+
 function App() {
-
-
-
-  let myname = "Ranjan Maji"
-  let age = 25;
-  let gender = "Male"
   return (
     <>
-      {/* <data1.Provider value={age}>
-        <data2.Provider value={myname}>
-          <data3.Provider value={gender}>
-            <ChildC></ChildC>
-
-          </data3.Provider>
-
-        </data2.Provider>
-
-      </data1.Provider>
-
-    </> */}
-      {/* <Forms /> */}
-      {/* <UseState /> */}
-      <UseCallback />
-
-
+      <h1>App Components</h1>
     </>
-
   )
-
 }
 
-export default App;
-
-export { data1, data2, data3 }
+export default App
