8a7d9fe92e7aa67549f1f8ef46c9eb0d30eb76c9
Ranjan02/ <ranjanmaji098@gmail.com>
2023-10-27 23:53:51 +0530

    add pinchaker

--- context-api/src/main.jsx
+++ context-api/src/main.jsx
@@ -30,7 +30,6 @@ import Contact from './components/Contact/Contact.jsx'
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />}>
-
       <Route path='' element={<Home />} />
       <Route path='/about' element={<About />} />
       <Route path='/contact' element={<Contact />} />
--- pinCodechake/src/App.jsx
+++ pinCodechake/src/App.jsx
@@ -1,27 +1,20 @@
 
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css'
 
 function App() {
-  const [pincode, setPincode] = useState(Number)
-  const [output, setOutput] = useState("")
-  const [data, setData] = useState([])
+  const [pincode, setPincode] = useState(1234)
+  const [result, setResult] = useState([])
 
-  useEffect((pincode) => {
 
-    fetch(`https://api.postalpincode.in/pincode/${pincode}`).then((result) => {
-      result.json().then((resp) => {
-        setData(resp.PostOffice)
+  async function hendleSubmit() {
+    let result = await fetch(`https://api.postalpincode.in/pincode/${pincode}`)
+    result = await result.json()
+    setResult(result[0].PostOffice[0])
 
-      })
-    })
-  }, [])
-
-  console.log(data);
-
-  function hendleSubmit() {
-    setOutput(pincode)
   }
+
+
 
   return (
     <>
@@ -29,21 +22,13 @@ function App() {
       <input type="number" value={pincode} id='3' onChange={(e) => setPincode(e.target.value)} />
       <button onClick={hendleSubmit}>Submit</button>
 
-      <h3>Pincode is : {output}</h3>
+      <h3>Pincode is :{Name}</h3>
+      <h5>Post Office Name : 723133</h5>
+      <h5>State : West-Bengal</h5>
+      <h5>Distric : Purulia</h5>
+      <h5>Block : Raghunathpur</h5>
 
-      <div>
-        {
-          data.map((item) => (
-            <>
-              <h5>Post Office Name :{item.name}</h5>
-              {/* <h5>State : West-Bengal</h5>
-              <h5>Distric : Purulia</h5>
-              <h5>Block : Raghunathpur</h5> */}
-            </>
 
-          ))
-        }
-      </div>
     </>
   )
 }
